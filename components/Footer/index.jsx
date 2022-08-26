66c2d94e1d74c148fa95af2d43a29e74bd7d7e45
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-08-26 15:42:07 +0700

    feat: add cursor pointer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -7,8 +7,14 @@ function Footer() {
             <HStack w='100%' justify={'space-between'}>
                 <Text fontSize={{ base: 10, sm: 12, md: 16 }}>© TemanRaga 2022. All Rights Reserved.</Text>
                 <HStack spacing='20px' justify={'space-between'}>
-                    <Image src='linelogo.svg' />
-                    <Image src='facebooklogo.svg' />
+                    <Image src='linelogo.svg' onClick={() => {
+                        window.location.href = "https://linevoom.line.me/user/_dTuaIG7gAO4PrGBOjaqKHrldjjG9ti-nImejj8g?utm_medium=windows&utm_source=desktop&utm_campaign=OA_Profile";
+                    }} _hover={{
+                        cursor: 'pointer'
+                    }} />
+                    <Image src='facebooklogo.svg' _hover={{
+                        cursor: 'pointer'
+                    }} />
                     <Image src='instagramlogo.svg' />
                     <Image src='twitterlogo.svg' />
                 </HStack>
