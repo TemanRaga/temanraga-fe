b6f03ddb9cf4503b18d3a8dcb6f988c21f60acd0
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-08-27 13:01:13 +0700

    feat: add location geotagging

--- pages/event/index.jsx
+++ pages/event/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Stack,
   Container,
@@ -14,6 +14,29 @@ import {
 import { Card } from "../../common/components";
 
 export default function Event() {
+
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (!navigator.geolocation) {
+        setStatus('Geolocation is not supported by your browser');
+      } else {
+        setStatus('Locating...');
+        navigator.geolocation.getCurrentPosition((position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        }, () => {
+          setStatus('Unable to retrieve your location');
+        });
+      }
+    }
+    getLocation();
+  }, [])
+
   return (
     <Flex flexDirection="column">
       <Flex flexDirection="column" p="0px">
