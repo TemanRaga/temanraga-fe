3ed18b3cac67e52cbe3d9feee63a051d2cddafdb
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-08-27 15:12:54 +0700

    feat: integrate detail

--- pages/event/[slug].jsx
+++ pages/event/[slug].jsx
@@ -1,26 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  Stack,
-  Container,
-  VStack,
+  Link,
   Text,
-  Input,
   Flex,
-  Spacer,
-  Box,
-  Checkbox,
   Image,
   Button,
   HStack,
 } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
+import { localEnv, serverEnv } from "../../common/constant/env";
+import Router from "next/router";
+import Cookies from 'js-cookie';
 
 export default function EventDetail() {
+
+  const [name, setName] = useState();
+  const [creator, setCreator] = useState();
+  const [description, setDescription] = useState();
+  const [location, setLocation] = useState();
+  const [participants, setParticipants] = useState([]);
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    fetch(`${localEnv}/api/v1/events/${3}`, {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setName(data.data.name);
+        setCreator(data.data.created_by.name);
+        setDescription(data.data.description);
+        setLocation(data.data.location);
+        setParticipants(data.data.participants);
+        setImage(data.image);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
+
+  const handleRegister = () => {
+    fetch(`${localEnv}/api/v1/events/${3}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Cookies.get('access-temanraga')}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const handleDelete = () => {
+    fetch(`${localEnv}/api/v1/events/${1}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Cookies.get('access-temanraga')}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+        Router.push("/event");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
   return (
     <Flex my="50px" flexDirection="column" p="3%">
       <Flex>
         <Image
-          src="https://images.ctfassets.net/hrltx12pl8hq/4f6DfV5DbqaQUSw0uo0mWi/6fbcf889bdef65c5b92ffee86b13fc44/shutterstock_376532611.jpg?fit=fill&w=800&h=300"
+          src={image}
           alt="EventPicture"
           width="100%"
           height="400px"
@@ -39,22 +101,16 @@ export default function EventDetail() {
         flexDirection="column"
       >
         <Text fontSize="xl" fontWeight="semibold" mb="8px">
-          Turnamen Bola RT 04
+          {name}
         </Text>
 
         <HStack flexDirection="row" mb="20px" gap={0}>
-          <Text fontSize="lg">Event Organizer</Text>
+          <Text fontSize="lg">{creator}</Text>
           <Icon icon="bi:check-circle-fill" margin="200px 200px" />
         </HStack>
 
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer odio
-          pharetra, risus, vel. Ornare enim auctor proin sed accumsan. Vitae
-          vulputate massa aliquam, viverra tempus vestibulum facilisis lorem.
-          Quam dolor congue morbi enim et enim suspendisse et eget. Ultricies
-          nibh massa scelerisque sed consequat diam. Sit nec malesuada egestas
-          mi elit libero ut tristique vulputate. Vel pharetra, auctor porttitor
-          nisi.
+          {description}
         </Text>
 
         <Flex
@@ -65,7 +121,7 @@ export default function EventDetail() {
           <Flex flexDirection="column">
             <HStack gap={4} flexDirection="row">
               <Icon icon="ci:location" />
-              <Text>Japan</Text>
+              <Text>{location}</Text>
             </HStack>
 
             <HStack gap={4} flexDirection="row">
@@ -77,7 +133,7 @@ export default function EventDetail() {
           <Flex flexDirection="column">
             <HStack gap={4} flexDirection="row">
               <Icon icon="bi:person-fill" />
-              <Text>20</Text>
+              <Text>{participants && participants.length} / Max</Text>
             </HStack>
 
             <HStack gap={4} flexDirection="row">
@@ -92,13 +148,15 @@ export default function EventDetail() {
           mt="3%"
           flexDirection={{ sm: "column", md: "column", lg: "row" }}
         >
-          <Button colorScheme="blue" variant="outline">
+          <Button colorScheme="blue" onClick={handleRegister}>
             Ikutan Aktivitas
           </Button>
-          <Button colorScheme="blue" variant="outline">
-            Edit Aktivitas
-          </Button>
-          <Button colorScheme="red" variant="outline">
+          <Link href='/edit'>
+            <Button colorScheme="blue" variant="outline">
+              Edit Aktivitas
+            </Button>
+          </Link>
+          <Button colorScheme="red" variant="outline" onClick={handleDelete}>
             Hapus Aktivitas
           </Button>
         </Flex>
