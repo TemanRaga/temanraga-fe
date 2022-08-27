027e378785737705cb3a5bd7d5c37945aabc161f
mbiskho <khomeini.bisma@gmail.com>
2022-08-27 17:23:55 +0700

    [BugFix] remove "www" on serverEnv

--- common/constant/env.jsx
+++ common/constant/env.jsx
@@ -1,4 +1,4 @@
 export const localEnv =
   process.env.NEXT_PUBLIC_LOCAL || "http://localhost:8000";
 export const serverEnv =
-  process.env.NEXT_PUBLIC_SERVER || "https://www.temanraga.xyz";
+  process.env.NEXT_PUBLIC_SERVER || "https://temanraga.xyz";
