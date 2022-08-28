4b67ad450f2cd73e240bab78e1dbaad627b4ad6b
mbiskho <khomeini.bisma@gmail.com>
2022-08-28 12:01:14 +0700

    [Enchanment] add data for tips

--- common/constant/data.jsx
+++ common/constant/data.jsx
@@ -8,35 +8,24 @@ export const TipsData = [
       "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/02/track-running-water-exercise-drinking-1296x728-header.jpg?w=1575",
   },
   {
-    title: "27 Health and Nutrition",
+    title: "What is lift weights",
     paragraph:
-      "Sugary drinks like sodas, fruit juices, and sweetened teas are the primary source of added sugar in the American diet. Some people avoid nuts because they are high in fat. However, nuts and seeds are incredibly nutritious. They are packed with protein, fiber, and a variety of vitamins and minerals",
-    date: "2 April 2018",
-    picture:
-      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/02/track-running-water-exercise-drinking-1296x728-header.jpg?w=1575",
+      "Your metabolism will actually go down, making weight loss more difficult. Resistance training, however, builds muscle to increase your metabolic rate.",
+    date: "30 July 2017",
+    picture: "https://api.time.com/wp-content/uploads/2016/01/metal-weights-4-fitness-exercise-strength-health-betterment-body-image.jpg?quality=85&w=1600"
   },
   {
-    title: "27 Health and Nutrition",
+    title: "11 Workout Tips to Make Your Exercises More Effective",
     paragraph:
-      "Sugary drinks like sodas, fruit juices, and sweetened teas are the primary source of added sugar in the American diet. Some people avoid nuts because they are high in fat. However, nuts and seeds are incredibly nutritious. They are packed with protein, fiber, and a variety of vitamins and minerals",
-    date: "2 April 2018",
-    picture:
-      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/02/track-running-water-exercise-drinking-1296x728-header.jpg?w=1575",
+      "There’s a lot of information available about heart rate zones, fitness gadgets and programs that promise results, but you don’t need to be an expert to achieve your goals. ",
+    date: "25 January 2022",
+    picture: "https://images.hindustantimes.com/img/2022/07/31/550x309/pexels-karolina-grabowska-4498551_1659256925866_1659256964708_1659256964708.jpg"
   },
   {
-    title: "27 Health and Nutrition",
+    title: "5 Steps to Follow for a Post-Workout Routine That Gets Results",
     paragraph:
-      "Sugary drinks like sodas, fruit juices, and sweetened teas are the primary source of added sugar in the American diet. Some people avoid nuts because they are high in fat. However, nuts and seeds are incredibly nutritious. They are packed with protein, fiber, and a variety of vitamins and minerals",
-    date: "2 April 2018",
-    picture:
-      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/02/track-running-water-exercise-drinking-1296x728-header.jpg?w=1575",
-  },
-  {
-    title: "27 Health and Nutrition",
-    paragraph:
-      "Sugary drinks like sodas, fruit juices, and sweetened teas are the primary source of added sugar in the American diet. Some people avoid nuts because they are high in fat. However, nuts and seeds are incredibly nutritious. They are packed with protein, fiber, and a variety of vitamins and minerals",
-    date: "2 April 2018",
-    picture:
-      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/02/track-running-water-exercise-drinking-1296x728-header.jpg?w=1575",
+      "What you do after your workout is an important part of yielding results, such as muscle gain and weight loss, while reducing muscle soreness.",
+    date: "30 July 2017",
+    picture: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/05/Male_Stretching_1296x728-header.jpg?w=1155&h=1528"
   },
 ];
